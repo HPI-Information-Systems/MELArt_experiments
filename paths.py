86fa27ff10b78db833c614bc93473ff501be7568
alejandro.sierra <alejandro.sierra@hpi.de>
2024-09-27 16:28:13 +0200

    New paths and candidate structure

--- paths.py
+++ paths.py
@@ -1,17 +1,17 @@
 from pathlib import Path
 import os
 
-BASE_MELART_PATH = # Path to the MelArt dataset (output_dir of the MelArt dataset)
+BASE_MELART_PATH = Path.home() / "MELArt/output_files"
 
-ARTPEDIA_IMAGES_PATH = # Path to the Artpedia images
+MELART_IMAGES_PATH = BASE_MELART_PATH / "images" / "files"
 
-CURATED_ANNOTATIONS_PATH = BASE_MELART_PATH / "input_files" / 'curated_annotations.json'
+#CURATED_ANNOTATIONS_PATH = BASE_MELART_PATH / "input_files" / 'curated_annotations.json'
 
-CANDIDATES_FOLDER_PATH = BASE_MELART_PATH / "output_files" / "el_candidates"
+CANDIDATES_FOLDER_PATH = BASE_MELART_PATH / "el_candidates"
 
-CANDIDATE_TYPES_DICT_PATH = BASE_MELART_PATH / "aux_files" / "candidate_types_dict.json"
+#CANDIDATE_TYPES_DICT_PATH = BASE_MELART_PATH / "aux_files" / "candidate_types_dict.json"
 
-COMBINED_ANNOTATIONS_PATH = BASE_MELART_PATH / "output_files" / "melart_annotations.json"
+COMBINED_ANNOTATIONS_PATH = BASE_MELART_PATH / "melart_annotations.json"
 
 OUTPUT_DATASETS_PATH = Path( "./output_datasets/" )
 os.makedirs(OUTPUT_DATASETS_PATH, exist_ok=True)
